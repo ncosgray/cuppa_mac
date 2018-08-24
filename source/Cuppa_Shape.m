cd78ed1add3c2aab1c4cb24c84602d48ed3753cb
Nathan Cosgray <ncosgray@gmail.com>
2018-08-24 10:18:31 -0700

    copyright year bump to 2018

--- source/Cuppa_Shape.m
+++ source/Cuppa_Shape.m
@@ -4,7 +4,7 @@
  Class:    Cuppa_Shape
            - Constants for cup shape types and associated abstract support class.
  ----------------------------------------------------------------------------------------------------
- Copyright (c) 2005-2017 Nathan Cosgray. All rights reserved.
+ Copyright (c) 2005-2018 Nathan Cosgray. All rights reserved.
  
  Redistribution and use in source and binary forms, with or without modification, are permitted
  provided that the following conditions are met:
--- source/main.m
+++ source/main.m
@@ -3,7 +3,7 @@
  Package:  Cuppa
  Class:    main
  ----------------------------------------------------------------------------------------------------
- Copyright (c) 2005-2017 Nathan Cosgray. All rights reserved.
+ Copyright (c) 2005-2018 Nathan Cosgray. All rights reserved.
  
  Redistribution and use in source and binary forms, with or without modification, are permitted
  provided that the following conditions are met:
