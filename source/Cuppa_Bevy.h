80014f9f73b33c59a89a6133b6eea57279ac181b
ncosgray <ncosgray@gmail.com>
2019-03-31 16:16:07 -0700

    bump copyright year in source

--- source/Cuppa_Bevy.h
+++ source/Cuppa_Bevy.h
@@ -4,7 +4,7 @@
  Class:    Cuppa_Bevy
            - The Bevy class tracks information about a single beverage, such as name and brew time.
  ----------------------------------------------------------------------------------------------------
- Copyright (c) 2005-2018 Nathan Cosgray. All rights reserved.
+ Copyright (c) 2005-2019 Nathan Cosgray. All rights reserved.
  
  Redistribution and use in source and binary forms, with or without modification, are permitted
  provided that the following conditions are met:
